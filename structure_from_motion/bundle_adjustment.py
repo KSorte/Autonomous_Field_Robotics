96b8975cd528ffbf848c41e76c9d670f50a0a985
Kaushal Sorte <kaushalsorte08@gmail.com>
2024-11-20 19:22:17 -0500

    Add the scale adjustment pipeline [not working]

--- structure_from_motion/bundle_adjustment.py
+++ structure_from_motion/bundle_adjustment.py
@@ -3,6 +3,7 @@ import gtsam.utils.plot as gtsam_plot
 import matplotlib.pyplot as plt
 import numpy as np
 import image_registration as ir
+import cv2
 from gtsam import (Cal3_S2,
                          NonlinearFactorGraph,Point3,
                          Pose3, PriorFactorPoint3, PriorFactorPose3, Rot3, Values)
@@ -51,16 +52,15 @@ class GTSAMBundleAdjustment:
 
         num_landmarks = len(self.all_landmarks_count)
         # Start with an identity pose.
-        current_pose = np.eye(4)
+        current_cam_extrinsic_pose = np.eye(4)
 
-        # Add Initial pose estimates for i and i+1 views.
-        self.initial.insert(X(0), gtsam.Pose3(current_pose))
+        # Add Initial pose estimates for 0 view.
+        self.initial.insert(X(0), gtsam.Pose3(current_cam_extrinsic_pose))
 
         for i in range(len(self.img_reg_obj.images)-1):
+            previous_cam_extrinsic_pose = current_cam_extrinsic_pose
             # Get i+1th camera extrinsics.
-            camera_projection = self.img_reg_obj.camera_extrinsic_poses[i+1]
-            # Get i+1th camera pose.
-            camera_pose = ir.ImageRegistration.get_transformation_matrix(camera_projection)
+            current_cam_extrinsic_pose = self.img_reg_obj.camera_extrinsic_poses[i+1]
 
             # Get i and i+1th view keypoints.
             src_points = self.img_reg_obj.inlier_points[i][0]
@@ -83,25 +83,62 @@ class GTSAMBundleAdjustment:
                 if landmark_index_src != -1:
                     keypoints_assigned_before_src.append(src_point)
                     keypoints_assigned_before_dst.append(dst_point)
+                    # Store current average of the repeated landmark points
                     averages_landmarks_found_before.append(self.all_landmarks_averages[landmark_index_src])
 
             # print("Common landmarks in ith", len(averages_landmarks_found_before))
-            # TODO (KSorte): Find the number of matched features needed for stable triangulation. Assigning 5 right now.
-            if (len(averages_landmarks_found_before) > 5):
-                # Convert to numpy arrays.
-                keypoints_assigned_before_src = np.array(keypoints_assigned_before_src)
-                keypoints_assigned_before_dst = np.array(keypoints_assigned_before_dst)
-                averages_landmarks_found_before = np.array(averages_landmarks_found_before)
-                # print("keypoints shape averaging", keypoints_assigned_before_src.shape)
-                cam_i = self.img_reg_obj.camera_extrinsic_poses[i]
-                cam_i_1 = self.img_reg_obj.camera_extrinsic_poses[i+1]
-                # print("Re triangulating...")
-                retriagulated_landmarks = \
-                    ir.ImageRegistration.triangulate_landmarks(cam_i, cam_i_1,
-                                                               keypoints_assigned_before_src,
-                                                               keypoints_assigned_before_dst,
-                                                               self.img_reg_obj.camera_intrinsics)
+            # # TODO (KSorte): Find the number of matched features needed for stable triangulation. Assigning 5 right now.
+            # if (len(averages_landmarks_found_before) > 0):
+            #     # Convert to numpy arrays.
+            #     keypoints_assigned_before_src = np.array(keypoints_assigned_before_src)
+            #     keypoints_assigned_before_dst = np.array(keypoints_assigned_before_dst)
+            #     # N x 3
+            #     averages_landmarks_found_before = np.array(averages_landmarks_found_before)
 
+            #     retriangulated_landmarks = \
+            #         ir.ImageRegistration.triangulate_landmarks(previous_cam_extrinsic_pose,
+            #                                                    current_cam_extrinsic_pose,
+            #                                                    keypoints_assigned_before_src,
+            #                                                    keypoints_assigned_before_dst,
+            #                                                    self.img_reg_obj.camera_intrinsics)
+
+            #     retriangulated_landmarks = (retriangulated_landmarks[0:3, :]).T
+
+            #     # # Compute the scale difference.
+            #     scale = 0
+            #     for p in range(retriangulated_landmarks.shape[0]):
+            #         scale += \
+            #         cv2.norm(averages_landmarks_found_before[p, :])/cv2.norm(retriangulated_landmarks[p, :])
+
+            #     scale /= retriangulated_landmarks.shape[0]
+            #     print("Scale = ", scale)
+
+            #     # Get R, T from relative pose b/w i and i+1th view.
+            #     rotation, translation = self.img_reg_obj.relative_poses[i]
+
+            #     # Adjust the translation scale.
+            #     translation *= scale
+
+            #     # Get refined SE3 relative pose
+            #     refined_relative_pose = np.eye(4)
+            #     refined_relative_pose[:3, :3] = rotation
+            #     refined_relative_pose[:3, 3] = translation.flatten()
+
+            #     # Use refined translation to get new camera extrinsics.
+            #     current_cam_extrinsic_pose = refined_relative_pose@previous_cam_extrinsic_pose
+
+            #     # Update ImageRegistration object
+            #     self.img_reg_obj.camera_extrinsic_poses[i+1] = current_cam_extrinsic_pose
+
+            #     # Use scale adjusted dst (i+1) view to retriangulate all points.
+            #     self.img_reg_obj.world_points_3D[i] = ir.ImageRegistration.triangulate_landmarks(previous_cam_extrinsic_pose,
+            #                                                                                      current_cam_extrinsic_pose,
+            #                                                                                      src_points, dst_points,
+            #                                                                                      self.img_reg_obj.camera_intrinsics)
+
+            #  Get i+1th camera pose.
+            camera_pose = ir.ImageRegistration.get_transformation_matrix(current_cam_extrinsic_pose)
+            self.initial.insert(X(i+1), gtsam.Pose3(camera_pose))
             overlapping_landmarks = 0
             # Iterate over matched keypoints for the i-i+1 views.
             for j, (src_point, dst_point, src_point_index, dst_point_index) in enumerate(zip(src_points,
@@ -136,7 +173,7 @@ class GTSAMBundleAdjustment:
                     # new landmark appears in ith (src) and i+1th (dst) images.
                     self.all_landmarks_count.append(2)
                 else:
-                    # print("Existing landmark found = ", landmark_index_src)
+
                     # Matched feature pair associated to an existing landmark.
                     self.img_reg_obj.object_index_list[i+1][dst_point_index] = landmark_index_src
 
@@ -161,8 +198,6 @@ class GTSAMBundleAdjustment:
             print(f'Number of landmarks in views {i} and {i+1} is {src_points.shape[0]}')
             print(f'Number of landmarks in views {i} and {i+1} found before is {overlapping_landmarks}')
             print(f'Number of landmarks after considering views {i} and {i+1} is {num_landmarks}')
-
-            self.initial.insert(X(i+1), gtsam.Pose3(camera_pose))
 
     def optimize_graph(self):
         params = gtsam.LevenbergMarquardtParams()
